175874cfeb4d38be9f9d5ddebb68f43dbbf94f8d
shin seung woo <ssw030209@gmail.com>
2025-02-27 17:04:05 +0900

    반복문 예제 2

--- ch4_loop/src/loop/WhileEx2 .java
+++ ch4_loop/src/loop/WhileEx2 .java
@@ -1,16 +1,21 @@
 package loop;
 
-public class ForEx2 {
+public class WhileEx2 {
     
     public static void main(String[] args){
-        for (int i = 1; i < 11; i++) {
-            System.out.print(i+"     ");
+        
+        
+        int i = 1;
+        While (i < 11){
+            System.out.print(i + "   ")
+
         }
         System.out.println();
 
         // 10~1
-        for (int i = 10; i > 0; i++) {
+         while ( i = 10); i > 0; i++) {
             System.out.print(i+"    ");
+
         }
 
         // 짝수만 출력 1~10
@@ -20,12 +25,23 @@ public class ForEx2 {
             System.out.print(i+"    ");
          }
       }
-
-      System.out.println();
-      
+      i = 1;
+      while (i < 11) {
+        if ()    
+            System.out.println();
+        }
       // i+=2
       for (int i = 1; i < 11; i++) {
-            System.out.print(i + "   ");
+        
+    }
+        
+            System.out.print();
+
+            i = 2;
+            while (i < 11) {
+                System.out.print(i + "  ");
+                i += 2;
+            }
       }
    }
 }
