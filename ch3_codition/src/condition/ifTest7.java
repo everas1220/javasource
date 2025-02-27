20b7ebd5e4698d38d74b6fc6d4a66992eaff100f
shin seung woo <ssw030209@gmail.com>
2025-02-27 14:34:44 +0900

    조건문 예제 완성

---
+++ ch3_codition/src/condition/ifTest7.java
@@ -0,0 +1,19 @@
+package condition;
+
+public class ifTest7 {
+   public static void main(String[] args) {
+      //num1 이라는 변수 선언후 임의의 값 담기
+
+      //num1 이 짝수, 홀수인지 출력하기 
+
+      int num1 = 77;
+
+      //num1 이 짝수,홀수인지출력하기
+      //num1 %2== -\0
+      if (num % 2 == 0) {
+         System.out.println("짝수");
+      } else 
+         System.out.println("홀수";
+   }
+
+}
